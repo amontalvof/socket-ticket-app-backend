dca3b7bb911cfe2d33caad103276bc3da6aee962
amontalvof <amontalvof92@gmail.com>
2021-07-01 09:08:32 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Socket Ticket App Backend
+
+## Technologies
+
+-   Node.js
+-   Express.js
+-   Socket.io
+
+## Notes:
+
+Remember to rebuild the Node modules `npm install`
---
+++ models/sever.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const cors = require('cors');
+const colors = require('colors/safe');
+// Express Server
+const express = require('express');
+// HTTP Server
+const http = require('http');
+// Socket Server
+const socketIo = require('socket.io');
+const Sockets = require('./sockets');
+
+class Server {
+    constructor() {
+        this.port = process.env.PORT;
+        this.app = express();
+        this.server = http.createServer(this.app);
+        this.io = socketIo(this.server, {
+            /** configurations */
+        });
+    }
+
+    middlewares() {
+        // Display the public directory
+        this.app.use(express.static(path.resolve(__dirname, '../public')));
+        // CORS
+        this.app.use(cors());
+    }
+
+    socketsConfiguration() {
+        new Sockets(this.io);
+    }
+
+    execute() {
+        //Initialize Middlewares
+        this.middlewares();
+        // Initialize Sockets
+        this.socketsConfiguration();
+        //Initialize Server
+        this.server.listen(this.port, () => {
+            console.log(colors.cyan(`Server running on port ${this.port}`));
+        });
+    }
+}
+
+module.exports = Server;
